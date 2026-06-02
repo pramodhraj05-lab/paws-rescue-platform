b4346a14a9fcebfe8c8a4a61ac7835b0a2dd3faa
Pramodh S <20092022@mydbs.ie>
2026-06-02 14:27:41 +0100

    feat: wire auth routes into express app

--- server/server.js
+++ server/server.js
@@ -7,13 +7,17 @@ const connectDB = require("./db/connection");
 const app = express();
 
 // Connect to MongoDB
-connectDB();
+// connectDB();
 
 // Middleware
 app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
+
+// Routes
+const authRoutes = require("./routes/auth");
+app.use("/auth", authRoutes);
 
 // Health check route
 app.get("/", (req, res) => {
