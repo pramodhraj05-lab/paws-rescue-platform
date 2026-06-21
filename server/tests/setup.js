19279f9e0419489617fd0bb8cd038b4bd47ee857
Pramodh S <20092022@mydbs.ie>
2026-06-21 07:56:32 +0100

    ci: use MongoDB service container instead of in-memory binary

--- server/tests/setup.js
+++ server/tests/setup.js
@@ -1,4 +1,4 @@
-process.env.JWT_SECRET = "test_secret_for_jest_only";
+process.env.JWT_SECRET = process.env.JWT_SECRET || "test_secret_for_jest_only";
 
 const mongoose = require("mongoose");
 const { MongoMemoryServer } = require("mongodb-memory-server");
@@ -6,8 +6,14 @@ const { MongoMemoryServer } = require("mongodb-memory-server");
 let mongoServer;
 
 async function setupTestDB() {
-  mongoServer = await MongoMemoryServer.create();
-  await mongoose.connect(mongoServer.getUri());
+  if (process.env.MONGO_TEST_URI) {
+    // CI mode: use real MongoDB service
+    await mongoose.connect(process.env.MONGO_TEST_URI);
+  } else {
+    // Local mode: in-memory MongoDB
+    mongoServer = await MongoMemoryServer.create();
+    await mongoose.connect(mongoServer.getUri());
+  }
 }
 
 async function teardownTestDB() {
